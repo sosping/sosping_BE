92f7becdb2ac281c7688f625ca572f2c81263152
Jeong-Rae <kkwjdfo@gmail.com>
2024-05-17 21:17:08 +0900

    [Feat] 로그인 기능 추가

--- src/main/java/sosping/be/domain/member/controller/AuthController.java
+++ src/main/java/sosping/be/domain/member/controller/AuthController.java
@@ -1,9 +1,11 @@
 package sosping.be.domain.member.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import sosping.be.domain.member.domain.Member;
 import sosping.be.domain.member.dto.LogInRequest;
 import sosping.be.domain.member.dto.MemberResponse;
@@ -12,7 +14,8 @@ import sosping.be.domain.member.service.AuthService;
 import sosping.be.global.aspect.LogMonitoring;
 import sosping.be.global.jwt.TokenProvider;
 
-@Controller
+@RestController
+@Tag(name = "Auth")
 @RequiredArgsConstructor
 @RequestMapping("api/auth")
 public class AuthController {
@@ -21,6 +24,7 @@ public class AuthController {
     private final TokenProvider tokenProvider;
 
     @LogMonitoring
+    @Operation(summary = "회원가입")
     @PostMapping("signup")
     public MemberResponse signUp(SignUpRequest request) {
         Member member = authService.signup(request);
@@ -32,6 +36,7 @@ public class AuthController {
     }
 
     @LogMonitoring
+    @Operation(summary = "로그인")
     @PostMapping("login")
     public MemberResponse logIn(LogInRequest request) {
         Member member = authService.login(request);
